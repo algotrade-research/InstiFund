84909067827b63b77907760f79aa160dcc79c810
BJMinhNhut <nhutben678@gmail.com>
2025-03-31 19:36:02 +0700

    Add last close price of each quarter to the financial data to calculate P/E

--- src/crawler/stocks_crawler.py
+++ src/crawler/stocks_crawler.py
@@ -57,6 +57,19 @@ def execute_query(query: str, params: tuple = ()) -> List[Dict[str, Any]]:
             logger.debug("Database connection closed.")
 
 
+def get_last_close_price(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Retrieve the last close price of each stock at the end of each quarter.
+    """
+    df['quarter'] = df['datetime'].dt.to_period('Q') - 1
+    last_close_prices = df.sort_values(by=['datetime']).groupby(
+        ['tickersymbol', 'quarter']).last().reset_index()
+    last_close_prices = last_close_prices[['tickersymbol', 'quarter', 'price']]
+    last_close_prices.rename(
+        columns={'price': 'last_close_price'}, inplace=True)
+    return last_close_prices
+
+
 def get_daily_data(start_date: datetime, end_date: datetime) -> pd.DataFrame:
     """
     Get daily data from the database for the given date range.
@@ -97,43 +110,68 @@ if __name__ == "__main__":
 
     start_date = datetime.strptime(args.start_date, "%Y-%m-%d")
     end_date = datetime.strptime(args.end_date, "%Y-%m-%d")
+
+    # Retrieve close price data
     try:
         logger.info(
             f"Starting data retrieval for range: {start_date} to {end_date}.")
-        data = get_daily_data(start_date, end_date)
-        if data.empty:
+        daily_data = get_daily_data(start_date, end_date)
+        if daily_data.empty:
             logger.warning("No data found for the given date range.")
         else:
-            logger.info("Data retrieved successfully. Saving to CSV.")
-            print(data.head())
-            # Save to CSV
-            csv_path = os.path.join(DATA_PATH, "daily_data.csv")
-            data.to_csv(csv_path, index=False)
-            logger.info(f"Data saved to {csv_path}")
-    except Exception as e:
-        logger.error(f"An error occurred: {e}")
+            logger.info("Daily data retrieved successfully.")
+            print(daily_data.head())
 
-    # financial data
+            # Save to CSV
+            daily_data.to_csv(os.path.join(
+                DATA_PATH, "daily_data.csv"), index=False)
+            logger.info("Daily data saved to CSV.")
+    except Exception as e:
+        logger.error(f"An error occurred while retrieving daily data: {e}")
+
+    # Retrieve financial data and add last close price
     stocks_list = get_stocks_list()
     try:
         logger.info(
             f"Starting financial data retrieval for range: {start_date} to {end_date}.")
-        # Get the list of stocks to filter
         if not stocks_list:
             logger.warning("No stocks found in the stock list.")
             raise ValueError("No stocks found in the stock list.")
 
         query = FINANCIAL_QUERY
         params = (start_date.year, end_date.year, stocks_list)
-        results = execute_query(query, params)
+        financial_results = execute_query(query, params)
 
-        if not results:
+        if not financial_results:
             logger.warning("No financial data found for the given date range.")
         else:
-            df = pd.DataFrame(results)
-            df.to_csv(os.path.join(
-                DATA_PATH, "financial_data.csv"), index=False)
-            logger.info("Financial data retrieved successfully. Saved to CSV.")
+            logger.info("Financial data retrieved successfully.")
+            financial_df = pd.DataFrame(financial_results)
+
+            # Add last close price as a new row for each stock and quarter
+            logger.info("Calculating last close prices.")
+            last_close_prices = get_last_close_price(daily_data)
+            rows_to_add = []
+            for _, row in last_close_prices.iterrows():
+                rows_to_add.append({
+                    "tickersymbol": row["tickersymbol"],
+                    "year": row["quarter"].year,
+                    "quarter": row["quarter"].quarter,
+                    "name": "Last Close Price",
+                    "value": row["last_close_price"]
+                })
+
+            # Convert rows_to_add to a DataFrame and concatenate with financial_df
+            rows_to_add_df = pd.DataFrame(rows_to_add)
+            financial_df = pd.concat(
+                [financial_df, rows_to_add_df], ignore_index=True)
+
+            # Save to CSV
+            print(financial_df.head())
+            financial_df.to_csv(os.path.join(
+                DATA_PATH, "financial_data_with_close_price.csv"), index=False)
+            logger.info("Financial data with last close price saved to CSV.")
     except Exception as e:
-        logger.error(f"An error occurred: {e}")
+        logger.error(f"An error occurred while retrieving financial data: {e}")
+
     logger.info("Crawler finished running.")
